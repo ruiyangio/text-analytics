eb00781dbb9b2ab628252e0543bbc02078f12f00
ruiyangio <ruiyangwind@gmail.com>
2018-06-24 11:28:25 -0700

    Add negation handling

--- common/util.py
+++ common/util.py
@@ -47,4 +47,34 @@ class Util(object):
     @staticmethod
     def defaultTokenize(text):
         text = Util.normalizeString(text)
-        return re.split(r"\s+", text)
+        # return re.split(r"\s+", text)
+        return Util.negate_sequence(text)
+
+    @staticmethod
+    def negate_sequence(text):
+        negation = False
+        delims = "?.,!:;"
+        result = []
+        words = text.split()
+        prev = None
+        pprev = None
+        for word in words:
+            stripped = word.strip(delims).lower()
+            negated = "not_" + stripped if negation else stripped
+            result.append(negated)
+            if prev:
+                bigram = prev + " " + negated
+                result.append(bigram)
+                if pprev:
+                    trigram = pprev + " " + bigram
+                    result.append(trigram)
+                pprev = prev
+            prev = negated
+
+            if any(neg in word for neg in ["not", "n't", "no"]):
+                negation = not negation
+
+            if any(c in word for c in delims):
+                negation = False
+
+        return result
